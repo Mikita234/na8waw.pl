8bfdaad55a89e480cf6d53c434000629bdb60564
Mikita234 <mikita234@example.com>
2026-04-10 13:57:20 +0200

    Make root Russian and restyle countdown board

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -1,6 +1,6 @@
 import "./predictions.js?v=20260409u";
 import "./daily.js?v=20260410d";
-import { siteCopy } from "./locale-data.js?v=20260410h";
+import { siteCopy } from "./locale-data.js?v=20260410i";
 import { subscribeLocale } from "./locale.js?v=20260409u";
 
 let programStatusInterval;
